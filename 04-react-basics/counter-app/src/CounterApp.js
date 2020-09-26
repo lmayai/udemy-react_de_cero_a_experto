ef7f8861da0b8a91e9ee3df555d8a772b958d9ba
Leonardo <leonardomayairegui@gmail.com>
2020-09-25 19:31:57 -0500

    handle subtract y reset

--- 04-react-basics/counter-app/src/CounterApp.js
+++ 04-react-basics/counter-app/src/CounterApp.js
@@ -1,21 +1,27 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
-const CounterApp = ({value}) => {
+const CounterApp = ({value = 10}) => {
 
-  const [counter, setCounter] = useState(0);
+  const [counter, setCounter] = useState(value);
 
   // handleAdd: usar la palabra handle es muy apropiado
   const handleAdd = (e) => {
-    // setCounter(counter+1)
-    setCounter( (c) => c + 1 )
+    setCounter(counter+1);
+    // setCounter( (c) => c + 1 )
   }
+
+  const handleReset = (e) => setCounter(value);
+  
+  const handleSubtract = (e) => setCounter(counter-1);
 
   return (
     <>
       <h1>CounterApp</h1>
       <h2> {counter} </h2>
-      <button onClick={ handleAdd }>+</button>
+      <button onClick={ handleAdd }> + </button>
+      <button onClick={ handleReset }> Reset </button>
+      <button onClick={ handleSubtract }> - </button>
     </>
   )
 }
